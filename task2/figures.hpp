0409b72161bec841d45c73f3634ca48ff6fde265
Sergey Ilinykh <62497831+un4g1vable@users.noreply.github.com>
2024-11-10 23:14:43 +0500

    Update figures.hpp

--- task2/figures.hpp
+++ task2/figures.hpp
@@ -1,10 +1,10 @@
 #pragma once
-
 #include <memory>
 #include <stdexcept>
 
-enum class FigureType
-{
+static constexpr double PI = 3.14;
+
+enum class FigureType {
     TRIANGLE,
     CIRCLE,
     RECTANGLE,
@@ -15,27 +15,51 @@ public:
     virtual FigureType Type() const = 0;
     virtual double Perimeter() const = 0;
     virtual double Area() const = 0;
+    virtual ~Figure() = default;
 };
 
-class Rect : public Figure
-{
+class Rect : public Figure {
+public:
+    Rect(double width, double height);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
+
+private:
+    double width;
+    double height;
 };
 
-class Triangle : public Figure
-{
+class Triangle : public Figure {
+public:
+    Triangle(double a, double b, double c);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
+
+private:
+    double a, b, c;
 };
 
-class Circle : public Figure
-{
+class Circle : public Figure {
+public:
+    Circle(double radius);
+    FigureType Type() const override;
+    double Perimeter() const override;
+    double Area() const override;
+
+private:
+    double radius;
 };
 
 std::unique_ptr<Figure> make_figure(FigureType type, double a, double b = 0, double c = 0);
 
-class WrongTriangle : public std::invalid_argument
-{
+class WrongTriangle : public std::invalid_argument {
+public:
+    WrongTriangle() : std::invalid_argument("") {}
 };
 
-class LessThanZeroParam : public std::invalid_argument
-{
+class LessThanZeroParam : public std::invalid_argument {
+public:
+    LessThanZeroParam() : std::invalid_argument("") {}
 };
-
