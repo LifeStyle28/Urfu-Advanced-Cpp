0dfe149c0a53bec90272c2304843235e35c3007f
ecl1pse192 <abobus2029@gmail.com>
2024-11-15 22:11:53 +0500

    task3

--- task3/circular_queue.cpp
+++ task3/circular_queue.cpp
@@ -1,36 +1,47 @@
 #include "circular_queue.hpp"
+#include <stdexcept>
 
-CircularQueue::CircularQueue(size_t size)
-{
-	// your implementation here
+CircularQueue::CircularQueue(size_t size) : capacity(size), head(0), tail(0), size(0) {
+    buffer = new int[size];
 }
 
-bool CircularQueue::Push(int value)
-{
-    // your implementation here
+bool CircularQueue::Push(int value) {
+    if (Full()) {
+        return false;
+    }
+    buffer[tail] = value;
+    tail = (tail + 1) % capacity;
+    size++;
+    return true;
 }
 
-bool CircularQueue::Pop()
-{
-    // your implementation here
+bool CircularQueue::Pop() {
+    if (Empty()) {
+        return false;
+    }
+    head = (head + 1) % capacity;
+    size--;
+    return true;
 }
 
-int CircularQueue::Front() const
-{
-    // your implementation here
+int CircularQueue::Front() const {
+    if (Empty()) {
+        return -1;
+    }
+    return buffer[head];
 }
 
-int CircularQueue::Back() const
-{
-    // your implementation here
+int CircularQueue::Back() const {
+    if (Empty()) {
+        return -1;
+    }
+    return buffer[(tail - 1 + capacity) % capacity];
 }
 
-bool CircularQueue::Empty() const
-{
-    // your implementation here
+bool CircularQueue::Empty() const {
+    return size == 0;
 }
 
-bool CircularQueue::Full() const
-{
-    // your implementation here
+bool CircularQueue::Full() const {
+    return size == capacity;
 }
