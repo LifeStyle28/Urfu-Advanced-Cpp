8074ca9380195b049cae3af520ea36d45fc6431c
Roman-Mingaleev-RI232702 <roman.mingaleev2001@gmail.com>
2024-11-19 22:48:17 +0500

    Update matrix.cpp

--- task1/matrix.cpp
+++ task1/matrix.cpp
@@ -4,79 +4,45 @@
 
 Matrix::Matrix(int numRows, int numCols)
 {
-if (numRows < 0 || numCols < 0) {
-throw std::out_of_range("Number of rows and columns must be non-negative");
-}
-
-data_.resize(numRows, std::vector<int>(numCols, 0));
+    // your implementation here
 }
 
 void Matrix::Reset(int numRows, int numCols)
 {
-if (numRows < 0 || numCols < 0) {
-throw std::out_of_range("Number of rows and columns must be non-negative");
-}
-
-
-data_.resize(numRows, std::vector<int>(numCols, 0));
+    // your implementation here
 }
 
 int& Matrix::At(int row, int col)
 {
-if (row < 0 || row >= data_.size() || col < 0 || col >= data_[0].size()) {
-throw std::out_of_range("Index out of range");
-}
-
-
-return data_[row][col];
+    // your implementation here
 }
 
 const int& Matrix::At(int row, int col) const
 {
-if (row < 0 || row >= data_.size() || col < 0 || col >= data_[0].size()) {
-throw std::out_of_range("Index out of range");
-}
-
-return data_[row][col];
+    // your implementation here
 }
 
 int Matrix::GetRows() const
 {
-return data_.size();
+    // your implementation here
 }
 
 int Matrix::GetCols() const
 {
-if (data_.empty()) {
-return 0;
-} else {
-return data_[0].size();
-}
+    // your implementation here
 }
 
 bool Matrix::operator==(const Matrix& m2)
 {
-return data_ == m2.data_;
+    // your implementation here
 }
 
 bool Matrix::operator!=(const Matrix& m2)
 {
-return data_ != m2.data_;
+    // your implementation here
 }
 
 Matrix Matrix::operator+(const Matrix& m2)
 {
-if (data_.size() != m2.data_.size() || data_[0].size() != m2.data_[0].size()) {
-throw std::invalid_argument("Matrices must be of the same size for addition");
-}
-  
-Matrix result(data_.size(), data_[0].size());
-
-for (int i = 0; i < data_.size(); ++i) {
-    for (int j = 0; j < data_[0].size(); ++j) {
-        result.At(i, j) = data_[i][j] + m2.At(i, j);
-    }
-}
-
-return result;
+    // your implementation here
 }
