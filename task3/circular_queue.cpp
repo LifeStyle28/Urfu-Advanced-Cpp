d302f1ce9b711bca695f04581ef61007e7c58aa1
Nikita Shumov <schumov.nn@gmail.com>
2024-11-20 15:42:46 +0500

    Update circular_queue.cpp

--- task3/circular_queue.cpp
+++ task3/circular_queue.cpp
@@ -1,27 +1,14 @@
-#include "circular_queue.hpp"
-
-CircularQueue::CircularQueue(size_t size) : 
-    capacity(size),
-    front(0),
-    rear(0),
-    is_empty(true)
-{
+CircularQueue::CircularQueue(size_t size) : capacity(size), front(0), rear(0), size(0) {
     buffer = new int[size];
-}
-
-CircularQueue::~CircularQueue() {
-    delete[] buffer;
 }
 
 bool CircularQueue::Push(int value) {
     if (Full()) {
         return false;
     }
-    
     buffer[rear] = value;
     rear = (rear + 1) % capacity;
-    is_empty = false;
-    
+    size++;
     return true;
 }
 
@@ -29,10 +16,8 @@ bool CircularQueue::Pop() {
     if (Empty()) {
         return false;
     }
-    
     front = (front + 1) % capacity;
-    is_empty = (front == rear);
-    
+    size--;
     return true;
 }
 
@@ -47,13 +32,13 @@ int CircularQueue::Back() const {
     if (Empty()) {
         return -1;
     }
-    return buffer[(rear == 0 ? capacity : rear) - 1];
+    return buffer[(rear - 1 + capacity) % capacity];
 }
 
 bool CircularQueue::Empty() const {
-    return is_empty;
+    return size == 0;
 }
 
 bool CircularQueue::Full() const {
-    return !is_empty && (front == rear);
+    return size == capacity;
 }
